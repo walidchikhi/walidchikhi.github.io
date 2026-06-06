2791d8072990ef9992073af6947626c056eaa543
Walid CHIKHI <waliidchikhi@gmail.com>
2026-06-06 13:23:01 +0100

    clean repo

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base: '/',          // ← add this line
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
